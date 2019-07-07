6211f782a3cfff3997db57e520f11856f155f5ef
lhm <laihuanminpc>
2019-07-07 12:11:20 +0800

    add ts

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
-import fs from 'fs';
-import path from 'path';
-import os from 'os';
-import process from 'process';
+import * as fs from 'fs';
+import * as path from 'path';
+import * as os from 'os';
+import * as process from 'process';
 var _ = require('lodash');
 var sh = require('shelljs');
 var gutils = require('global-code-utils');
