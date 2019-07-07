6c2469c9e8ca497941c6fcf8321d8bf70762a83d
lhm <laihuanminpc>
2019-07-07 16:04:47 +0800

    add readme, and add basic arg code

---
+++ README.md
@@ -0,0 +1,15 @@
+# web-control-panel
+用于前端开发依赖、构建、部署等工作的控制面板工具，使用TypeScript开发
+
+# 安装软件包
+```bash
+npm i -g web-control-panel
+```
+
+# 命令参数
+```
+wcp list-project # 列出已有的项目
+wcp new-project [dirpath] # 创建一个webpack的新项目
+wcp view # 启动后台服务，帮助你管理项目 
+```
+
--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,8 @@ import * as fs from 'fs';
 import * as path from 'path';
 import * as os from 'os';
 import * as process from 'process';
-import * as inquirer from 'inquirer'
-import * as ora from 'ora';
+import * as inquirer from 'inquirer';
+var ora = require('ora');
 var _ = require('lodash');
 var sh = require('shelljs');
 var gutils = require('global-code-utils');
@@ -12,8 +12,12 @@ function getCwdDir(targetPath) {
 	return path.join(process.cwd(), targetPath);
 }
 
-function plainlog(str) {
-	console.log(str);
+function isPathExists(targetPath) {
+	return fs.existsSync(targetPath);
+}
+
+function plainlog(...str) {
+	console.log(...str);
 }
 
 function getArgWithoutExec() {
@@ -23,6 +27,15 @@ function getArgWithoutExec() {
 function isEmptyOrHelpArg() {
 	var arr = getArgWithoutExec();
 	return _.isEmpty(arr) || _.first(arr) === 'help';
+}
+
+function exitProgram(code) {
+	return process.exit(code);
+}
+
+// crt project functions
+function createOra(msg: string) {
+	return ora(msg).start();
 }
 
 const helpText = `web-control-panel help
@@ -41,6 +54,7 @@ Github: https://github.com/tun100/
 Repository: https://github.com/tun100/web-control-panel`;
 
 async function entryfunc() {
+    plainlog(path.normalize(`~`));
 	if (isEmptyOrHelpArg()) {
 		// print help text
 		plainlog(helpText);
@@ -48,10 +62,22 @@ async function entryfunc() {
 		// start analyze arguments
 		let argArr: string[] = getArgWithoutExec();
 		let command = _.first(argArr);
+		let options = _.get(argArr, 1);
+		var msgref_init = createOra('initializing task...');
 		switch (command) {
 			case 'list-project':
 				break;
 			case 'new-project':
+                // check path
+				if (_.isNil(options)) {
+					options = getCwdDir('');
+				}
+				if (!isPathExists(options)) {
+					msgref_init.fail(`path doesn't exists, the path is ${options}`);
+					exitProgram(-1);
+				}
+				msgref_init.succeed(`new project path is ${options}`);
+                msgref_init.stop();
 				break;
 			case 'view':
 				break;
